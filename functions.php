eee1977d461b2f2218dc234fcbab2a87e57c9e88
victor <victor3790@gmail.com>
2024-08-12 13:45:13 -0600

    Sidebar added to archive pages

--- functions.php
+++ functions.php
@@ -87,6 +87,13 @@ function vcnt_load_assets() {
 			array( 'vcnt-style' ),
 			VCNT_VER
 		);
+
+		wp_enqueue_style(
+			'vcnt-sidebar',
+			VCNT_URI . '/css/sidebar.css',
+			array( 'vcnt-style' ),
+			VCNT_VER
+		);
 	}
 }
 
