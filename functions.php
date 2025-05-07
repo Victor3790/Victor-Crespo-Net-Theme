853d9c8172684970e7c49c59ad277518aec2d2d8
Victor <victor3790@gmail.com>
2025-05-06 18:40:46 -0600

    Custom post type and custom fields added.

--- functions.php
+++ functions.php
@@ -284,3 +284,136 @@ add_filter( 'the_category', 'vcnt_add_css_class_to_categories' );
 function vcnt_add_css_class_to_categories( $categories ) {
 	return str_replace( '<a ', '<a class="post__breadcrumbs-link " ', $categories );
 }
+
+add_action( 'init', 'vcnt_register_projects_cpt' );
+
+/**
+ * Register custom post type "Projects".
+ */
+function vcnt_register_projects_cpt() {
+	$labels = array(
+		'name'               => _x( 'Projects', 'post type general name', 'victor-crespo-net-theme' ),
+		'singular_name'      => _x( 'Project', 'post type singular name', 'victor-crespo-net-theme' ),
+		'menu_name'          => _x( 'Projects', 'admin menu', 'victor-crespo-net-theme' ),
+		'name_admin_bar'     => _x( 'Project', 'add new on admin bar', 'victor-crespo-net-theme' ),
+		'add_new'            => _x( 'Add New', 'project', 'victor-crespo-net-theme' ),
+		'add_new_item'       => __( 'Add New Project', 'victor-crespo-net-theme' ),
+		'new_item'           => __( 'New Project', 'victor-crespo-net-theme' ),
+		'edit_item'          => __( 'Edit Project', 'victor-crespo-net-theme' ),
+		'view_item'          => __( 'View Project', 'victor-crespo-net-theme' ),
+		'all_items'          => __( 'All Projects', 'victor-crespo-net-theme' ),
+		'search_items'       => __( 'Search Projects', 'victor-crespo-net-theme' ),
+		'parent_item_colon'  => __( 'Parent Projects:', 'victor-crespo-net-theme' ),
+		'not_found'          => __( 'No projects found.', 'victor-crespo-net-theme' ),
+		'not_found_in_trash' => __( 'No projects found in Trash.', 'victor-crespo-net-theme' ),
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'show_in_rest'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'projects' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+		'menu_icon'          => 'dashicons-portfolio',
+		'supports'           => array( 'title', 'editor', 'thumbnail', 'excerpt', 'custom-fields' ),
+	);
+
+	register_post_type( 'projects', $args );
+}
+
+add_action( 'add_meta_boxes', 'vcnt_add_projects_meta_boxes' );
+
+/**
+ * Add custom meta boxes for Projects post type.
+ */
+function vcnt_add_projects_meta_boxes() {
+	add_meta_box(
+		'vcnt_github_link',
+		__( 'GitHub Link', 'victor-crespo-net-theme' ),
+		'vcnt_render_github_link_meta_box',
+		'projects',
+		'normal',
+		'default'
+	);
+
+	add_meta_box(
+		'vcnt_wordpress_org_link',
+		__( 'WordPress.org Link', 'victor-crespo-net-theme' ),
+		'vcnt_render_wordpress_org_link_meta_box',
+		'projects',
+		'normal',
+		'default'
+	);
+}
+
+/**
+ * Render GitHub Link meta box.
+ *
+ * @param WP_Post $post The post object.
+ */
+function vcnt_render_github_link_meta_box( $post ) {
+	$value = get_post_meta( $post->ID, '_vcnt_github_link', true );
+	wp_nonce_field( 'vcnt_save_github_link', 'vcnt_github_link_nonce' );
+	?>
+	<label for="vcnt_github_link"><?php esc_html_e( 'GitHub Repository URL:', 'victor-crespo-net-theme' ); ?></label>
+	<input type="url" id="vcnt_github_link" name="vcnt_github_link" value="<?php echo esc_attr( $value ); ?>" style="width: 100%;" />
+	<?php
+}
+
+/**
+ * Render WordPress.org Link meta box.
+ *
+ * @param WP_Post $post The post object.
+ */
+function vcnt_render_wordpress_org_link_meta_box( $post ) {
+	$value = get_post_meta( $post->ID, '_vcnt_wordpress_org_link', true );
+	wp_nonce_field( 'vcnt_save_wordpress_org_link', 'vcnt_wordpress_org_link_nonce' );
+	?>
+	<label for="vcnt_wordpress_org_link"><?php esc_html_e( 'WordPress.org Plugin/Theme URL:', 'victor-crespo-net-theme' ); ?></label>
+	<input type="url" id="vcnt_wordpress_org_link" name="vcnt_wordpress_org_link" value="<?php echo esc_attr( $value ); ?>" style="width: 100%;" />
+	<?php
+}
+
+add_action( 'save_post_projects', 'vcnt_save_projects_meta_boxes', 10, 2 );
+
+/**
+ * Save custom meta box data for Projects post type.
+ *
+ * @param int     $post_id The ID of the post being saved.
+ * @param WP_Post $post The post object.
+ */
+function vcnt_save_projects_meta_boxes( $post_id, $post ) {
+	$test = $post;
+
+	$github_link_nonce = isset( $_POST['vcnt_github_link_nonce'] ) ? sanitize_key( wp_unslash( $_POST['vcnt_github_link_nonce'] ) ) : '';
+
+	if ( empty( $github_link_nonce ) || ! wp_verify_nonce( $github_link_nonce, 'vcnt_save_github_link' ) ) {
+		return;
+	}
+
+	$wp_org_link_nonce = isset( $_POST['vcnt_wordpress_org_link_nonce'] ) ? sanitize_key( wp_unslash( $_POST['vcnt_wordpress_org_link_nonce'] ) ) : '';
+	if ( empty( $wp_org_link_nonce ) || ! wp_verify_nonce( $wp_org_link_nonce, 'vcnt_save_wordpress_org_link' ) ) {
+		return;
+	}
+
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	if ( isset( $_POST['vcnt_github_link'] ) ) {
+		$github_link = sanitize_url( wp_unslash( $_POST['vcnt_github_link'] ) );
+		update_post_meta( $post_id, '_vcnt_github_link', $github_link );
+	}
+
+	if ( isset( $_POST['vcnt_wordpress_org_link'] ) ) {
+		$wp_org_link = sanitize_url( wp_unslash( $_POST['vcnt_wordpress_org_link'] ) );
+		update_post_meta( $post_id, '_vcnt_wordpress_org_link', $wp_org_link );
+	}
+}
